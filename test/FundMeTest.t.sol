707da579e07acef4d74d34bcbf54c3ba9959e016
Peter Hind <peter.hind47@gmail.com>
2024-01-09 21:50:33 +1300

    add AddsFunderToArrayOfFunders test

--- test/FundMeTest.t.sol
+++ test/FundMeTest.t.sol
@@ -42,4 +42,12 @@ contract FundMeTest is Test {
         uint256 amountFunded = fundMe.getAddressToAmountFunded(USER);
         assertEq(amountFunded, SEND_VALUE);
     }
+
+    function testAddsFunderToArrayOfFunders() public {
+        vm.prank(USER);
+        fundMe.fund{value: SEND_VALUE}();
+
+        address funder = fundMe.getFunder(0);
+        assertEq(funder, USER);
+    }
 }
